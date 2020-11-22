0049d6398241f5f409b085460662bf99dd461f87
Darren <darren__c@hotmail.com>
2020-11-22 17:57:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const request = fetch('http://restcountries.eu/rest/v2/name/portugal');
+
+console.log(request);
+
+//consuming promises
+
+const getCountryData = function (country) {
+  fetch(`http://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log(data[0].capital);
+      console.log(data[0].population);
+    })
+}
+
+getCountryData('canada');
+//watch  247 chaining promises again
